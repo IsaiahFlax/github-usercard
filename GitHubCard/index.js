6d64598676bfe60efecfcf9b619559c2234281a9
IsaiahFlax <flax.isaiah@gmail.com>
2020-01-23 22:00:35 -0700

    Update index.js
    DONE!

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -23,9 +23,73 @@
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
+const axiosPromise = axios
+.get("https://api.github.com/users/IsaiahFlax")
+.then(response => {
+  console.log('res', response)
+  cards.append(createNewCard(response.data))
+})
+const cards = document.querySelector('.cards')
 
-const followersArray = [];
+console.log('promise', axiosPromise)
+const friendsArray = ['tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell']
 
+  friendsArray.forEach(user => {
+    axios
+    .get(`https://api.github.com/users/${user}`)
+    .then(response => {
+      cards.append(createNewCard(response.data))
+  console.log('res', response)
+  })
+})
+  console.log(friendsArray)
+const createNewCard = user => {
+const card = document.createElement('div')
+const image = document.createElement('img')
+const cardInfo = document.createElement('div')
+const name = document.createElement('h3')
+const username = document.createElement('p')
+const located = document.createElement('p')
+const profile = document.createElement('p')
+const profileHref = document.createElement('a')
+const followers = document.createElement('p')
+const following = document.createElement('p')
+const bio = document.createElement('p')
+
+image.src = `${user.avatar_url}`
+name.textContent = `${user.name}`
+username.textContent = `${user.login}`
+located.textContent = `Location : ${user.location}`
+profile.textContent  = 'Profile: '
+profileHref.textContent = `${user.html_url}`
+profileHref.href = `${user.html_url}`
+followers.textContent = `Followers: ${user.followers}`
+following.textContent = `Following: ${user.following}`
+bio.textContent = `Bio: ${user.bio}`
+
+card.append(image)
+card.append(cardInfo)
+cardInfo.append(name)
+cardInfo.append(username)
+cardInfo.append(located)
+cardInfo.append(profile)
+profile.append(profileHref)
+cardInfo.append(followers)
+cardInfo.append(following)
+cardInfo.append(bio)
+
+card.classList.add('card')
+cardInfo.classList.add('card-info')
+name.classList.add('name')
+username.classList.add('username')
+
+
+return card
+}
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -52,4 +116,13 @@ const followersArray = [];
   justsml
   luishrd
   bigknell
+*/
+/* Step 5: Now that you have your own card getting added to the DOM, either 
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
+          , manually find some other users' github handles, or use the list found 
+          at the bottom of the page. Get at least 5 different Github usernames and add them as
+          Individual strings to the friendsArray below.
+          
+          Using that array, iterate over it, requesting data for each user, creating a new card for each
+          user, and adding that card to the DOM.
 */
